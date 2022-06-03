08ac0def7b28afb5d70eaa341ae6a1fb05d129cf
Thang cg <duongsag.hp11@gmail.com>
2022-06-03 09:09:53 +0700

    up

--- Circle.ts
+++ Circle.ts
@@ -1,10 +1,27 @@
 class Circle {
+    color: string = 'green';
+    radius: number = 20;
 
-    public radius: number;
-
-    constructor(radius: number) {
-        this.radius = radius
+    constructor(color:string,radius: number) {
+        this.radius = radius;
+        this.color = color;
     }
 }
 
-export default Circle;
+let circleList: Circle[] = [];
+
+
+circleList.push(new Circle("yellow", 10));
+
+circleList.push(new Circle("green", 15));
+
+circleList.push(new Circle("blue", 20));
+
+function showCircle(circle: Circle) {
+
+    console.log("Hình tròn này có màu " + circle.color + " và bán kính là " + circle.radius);
+  
+  }
+
+circleList.forEach(showCircle);
+  
